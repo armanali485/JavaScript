2a1697cae0c8a67d59afdc8b58e5dc2f5726e24d
armanali485 <armanaligithub@gmail.com>
2024-10-05 16:50:24 +0530

    javaScript

---
+++ Dom Methods/Dom Creat Method/JavaScript.js
@@ -0,0 +1,4 @@
+document.getElementById("pTag").insertAdjacentHTML("beforebegin","<h1>Inter Adjacent Html Before Begin</h1>");
+document.getElementById("pTag").insertAdjacentHTML("afterbegin","<h1>Inter Adjacent Html After Begin</h1>");
+document.getElementById("pTag").insertAdjacentHTML("beforeend","<h1>Inter Adjacent Html Before End</h1>");
+document.getElementById("pTag").insertAdjacentHTML("afterend","<h1>Inter Adjacent Html After End</h1>");
